97841bab175340b5c4c0f4a37abb124375058b7c
Hasan Nakib <hsnnkb.dev@gmail.com>
2022-08-07 09:22:11 +0100

    Iteration 5 - banner image for project details.
    Create production build,
    Add dynamic banner image for projects pages.

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,19 +1,39 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import { GatsbyImage } from "gatsby-plugin-image";
+import { graphql } from "gatsby";
 import * as styles from "../styles/project-details.module.css";
 
-export default function projectDetails() {
+export default function projectDetails({ data }) {
+  const { html } = data.markdownRemark;
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter;
   return (
     <Layout>
       <div className={styles.details}>
-        <h2>title</h2>
-        <h3>stack</h3>
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
         <div className={styles.featured}>
-         {/*<GatsbyImage image={''} /> */}
+         {<GatsbyImage image={featuredImg.childImageSharp.gatsbyImageData} />}
         </div>
-        {/* <div className={styles.html} dangerouslySetInnerHTML={''} /> */}
+        { <div className={styles.html} dangerouslySetInnerHTML={{__html: html}} /> }
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featuredImg {
+          childImageSharp {
+            gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }
+    }
+  }
+`
